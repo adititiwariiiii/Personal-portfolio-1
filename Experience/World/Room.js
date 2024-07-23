3f5c3683a052827127d9267d9a9d980d222d2fb5
adititiwariiiii <tripathi.aditi9990@gmail.com>
2024-07-24 02:35:15 +0530

    cameraa

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -41,7 +41,7 @@ export default class Room {
         const height = 1;
         const intensity = 0.5;
         const rectLight = new THREE.RectAreaLight(
-            0xffffff,
+            0,
             intensity,
             width,
             height
