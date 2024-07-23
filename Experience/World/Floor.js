b3cdfd318752c2fac2978d018e1768edf630be63
adititiwariiiii <tripathi.aditi9990@gmail.com>
2024-07-24 01:57:15 +0530

    cameraa

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -1,38 +1,33 @@
 import * as THREE from "three";
 import Experience from "../Experience.js";
+import { Scene } from "three";
+import GSAP from "gsap";
 
-export default class Floor {
-    constructor() {
+export default class Floor{
+    constructor(){
         this.experience = new Experience();
         this.scene = this.experience.scene;
 
         this.setFloor();
     }
 
-    setFloor() {
+    setFloor(){
         this.geometry = new THREE.PlaneGeometry(100, 100);
         this.material = new THREE.MeshStandardMaterial({
             color: 0xa68fe7,
-            side: THREE.DoubleSide, // Use DoubleSide to make the plane visible from both sides
+            side: THREE.BackSide,
         });
         this.plane = new THREE.Mesh(this.geometry, this.material);
-
-        // Add the plane to the scene
         this.scene.add(this.plane);
-
-        // Set rotation and position
         this.plane.rotation.x = Math.PI / 2;
         this.plane.position.y = -0.1;
-        this.plane.position.z = 1; // Move the plane closer to the camera
-
-        // Ensure the plane is rendered on top of other objects
-        this.plane.renderOrder = 1;
-
-        // Enable the plane to receive shadows
-        this.plane.receiveShadow = true;
+        this.plane.receiveShadow =  true;
     }
 
-    resize() {}
 
-    update() {}
+    resize(){}
+
+    update(){
+
+    }
 }
