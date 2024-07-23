e01e83c15be093f316e7134b77122cf992fb17a3
adititiwariiiii <tripathi.aditi9990@gmail.com>
2024-07-24 01:54:53 +0530

    cameraa

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,47 +1,48 @@
 import * as THREE from "three";
+
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+import Resources from "./Utils/Resources.js";
+import assets from "./Utils/assets.js";
+
 import Camera from "./Camera.js";
+import Theme from "./Theme.js";
+import Renderer from "./Renderer.js";
 
-export default class Experience {
-    constructor() {
-        this.sizes = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-            aspect: window.innerWidth / window.innerHeight,
-            frustrum: 75
-        };
+import World from "./World/World.js";
 
-        this.canvas = document.querySelector('canvas.webgl'); // Ensure this matches your actual canvas selector
-
+export default class Experience{
+    static instance;
+    constructor(canvas){
+        if(Experience.instance){
+            return Experience.instance;
+        }
+        Experience.instance = this;
+        this.canvas = canvas;
         this.scene = new THREE.Scene();
-
+        this.time = new Time();
+        this.sizes = new Sizes();
         this.camera = new Camera();
+        this.renderer = new Renderer();
+        this.resources = new Resources(assets);
+        this.theme = new Theme();
+        this.world = new World();
 
-        this.renderer = new THREE.WebGLRenderer({
-            canvas: this.canvas,
-            antialias: true
+        this.sizes.on("resize", () => { 
+            this.resize();
         });
-        this.renderer.setSize(this.sizes.width, this.sizes.height);
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-        this.animate();
-
-        window.addEventListener('resize', () => {
-            this.sizes.width = window.innerWidth;
-            this.sizes.height = window.innerHeight;
-            this.sizes.aspect = window.innerWidth / window.innerHeight;
-
-            this.camera.resize();
-            this.renderer.setSize(this.sizes.width, this.sizes.height);
-            this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+        this.time.on("update", () => { 
+            this.update();
         });
     }
-
-    animate() {
-        requestAnimationFrame(() => this.animate());
-
+    resize() {
+        this.camera.resize();
+        this.world.resize();
+        this.renderer.resize();
+    }
+    update() {
         this.camera.update();
-        this.renderer.render(this.scene, this.camera.perspectiveCamera);
+        this.world.update();
+        this.renderer.update();
     }
 }
-
-const experience = new Experience();
