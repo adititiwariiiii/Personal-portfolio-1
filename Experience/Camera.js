3571a5279233cd5c92d3d5955f8a8477a4532a88
adititiwariiiii <tripathi.aditi9990@gmail.com>
2024-07-24 00:31:39 +0530

    update4.7

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -23,8 +23,8 @@ export default class Camera{
             );
             this.scene.add(this.perspectiveCamera);
             this.perspectiveCamera.position.x = 29;
-            this.perspectiveCamera.position.y = 25;
-            this.perspectiveCamera.position.z = 15;
+            this.perspectiveCamera.position.y = 14;
+            this.perspectiveCamera.position.z = 12;
     }
 
     createOrthographicCamera(){
@@ -39,7 +39,7 @@ export default class Camera{
 
             this.orthographicCamera.position.y = 3.5;
             this.orthographicCamera.position.z = 5;
-            this.orthographicCamera.rotation.x = -Math.PI / 6;
+            this.orthographicCamera.rotation.x = -Math.PI / 10;
 
             this.scene.add(this.orthographicCamera);
 
